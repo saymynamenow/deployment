9c49603fc0c590b7849c9579cdc7e731f43338b9
Semanem <muhammadfiqriananda@student.telkomuniversity.ac.id>
2025-05-29 20:14:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ app.use("/auth", authRoute);
 app.use("/api/", authenticate, doRoute);
 
 app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+  console.log("Server is running on port 3000");
 });
